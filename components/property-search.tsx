84c51da16bef9dfeeaaf3bc2bd6979de988a4e50
justin <justin@redhq.com.au>
2025-02-26 12:23:12 +0400

    Update placeholder text in PropertySearch component for clarity

--- components/property-search.tsx
+++ components/property-search.tsx
@@ -60,7 +60,7 @@ const PropertySearch = () => {
                                     "mt-1 block w-full rounded-lg border-none bg-white/60 py-2 px-3 text-sm/6 text-white placeholder-white",
                                     "focus:outline-none data-[focus]:outline-2 data-[focus]:-outline-offset-2 data-[focus]:outline-white/25"
                                 )}
-                                placeholder="Enter Keywords"
+                                placeholder="Enter (e.g. city, neighborhood, or address)"
                             />
                         </div>
                     </Field>
