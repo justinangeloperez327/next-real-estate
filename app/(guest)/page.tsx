dd79242d998343ead8d228339a6ab2277830a90a
justin <justin@redhq.com.au>
2025-02-26 12:19:31 +0400

    Refactor Home component to replace <main> with <div> for layout consistency

--- app/(guest)/page.tsx
+++ app/(guest)/page.tsx
@@ -6,12 +6,12 @@ import Testimonials from "@/components/testimonials";
 export default async function Home() {
     return (
         <>
-            <main className="flex-1 flex flex-col gap-6 px-4">
+            <div className="flex-1 flex flex-col gap-6">
                 <Hero />
                 <Featured />
                 <Testimonials />
                 <CallToAction />
-            </main>
+            </div>
         </>
     );
 }
